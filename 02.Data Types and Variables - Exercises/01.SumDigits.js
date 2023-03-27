9f6081000c1f4b079bf9434707be0001dd63783d
vankataloz <vankataloz@gmail.com>
2023-03-27 11:05:25 +0300

    new solution

--- 02.Data Types and Variables - Exercises/01.SumDigits.js
+++ 02.Data Types and Variables - Exercises/01.SumDigits.js
@@ -1,9 +1,15 @@
 function sumDigits(num) {
-    let numAsString = String(num);
+  /*  let numAsString = String(num);
     let sum = 0;
     for (let i = 0; i < numAsString.length; i++) {
         let currentChar = Number(numAsString[i]);
         sum+=currentChar;
+    }
+    console.log(sum); */
+    let sum = 0;
+    let numberAsString = num.toString();
+    for (const char of numberAsString) {
+        sum += Number(char);
     }
     console.log(sum);
 }
